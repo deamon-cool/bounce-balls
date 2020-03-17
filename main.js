7d4467bba6244524cecae2a2ccb82beeee3f2d63
daemon-cool <damian.damian.tofik@gmail.com>
2020-03-17 18:13:30 +0100

    Added EvilCircle class constructor with its methods

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ function random(min,max) {
 }
 
 
+// Shape class costructor, It's a parent class
+
 function Shape(x,y,velx,vely,exists) {
     this.x = x;
     this.y = y;
@@ -19,6 +21,8 @@ function Shape(x,y,velx,vely,exists) {
     this.exists = exists;
 }
 
+
+// Ball class constructor, inherited by Shape
 
 function Ball(x, y, velx, vely, exists, color, size) {
     Shape.call(this, x, y, velx, vely, exists);
@@ -74,6 +78,80 @@ Ball.prototype.collisionDetect = function() {
         }
     }
 };
+
+
+// EvilCircle class constructor, ingerited by Shape
+function EvilCircle(x, y, exists) {
+    Shape.call(this, x, y, 20, 20, exists);
+
+    this.color = 'white';
+    this.size = 10;
+}
+
+Object.defineProperty(EvilCircle.prototype, 'constructor', {
+    value: EvilCircle,
+    enumerable: false,
+    writable: true
+});
+
+EvilCircle.prototype.draw = function() {
+    ctx.beginPath();
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = 3;
+    ctx.arc(this.x, this.y, this.size, 0, 2*Math.PI);
+    ctx.stroke();
+};
+
+EvilCircle.prototype.checkBounds = function() {
+    if ((this.x + this.size) >= width) {
+        this.x -= this.size;
+    }
+
+    if ((this.x - this.size) <= 0) {
+        this.x += this.size;
+    }
+
+    if ((this.y + this.size) >= height) {
+        this.y -= this.size;
+    }
+
+    if ((this.y - this.size) <= 0) {
+        this.y += this.size;
+    }
+};
+
+EvilCircle.prototype.setControls = function() {
+    let _this = this;
+    window.onkeydown = function(e) {
+        if (e.key === 'ArrowLeft') {
+            _this.x -= _this.velX;
+          } else if (e.key === 'ArrowRight') {
+            _this.x += _this.velX;
+          } else if (e.key === 'ArrowUp') {
+            _this.y -= _this.velY;
+          } else if (e.key === 'ArrowDown') {
+            _this.y += _this.velY;
+          }
+    };
+};
+
+EvilCircle.prototype.collisionDetect = function() {
+    for (let j = 0; j < balls.length; j++) {
+        if (balls[j].exists) {
+            const dx = this.x - balls[j].x;
+            const dy = this.y - balls[j].y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+
+            if (distance < this.size + balls[j].size) {
+                balls[j].exists = false;
+
+                // ADD CODE HERE
+
+            }
+        }
+    }
+};
+
 
 
 let balls = [];
